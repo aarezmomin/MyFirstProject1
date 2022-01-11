8fa3a22281458482d0927d4d50347aefed02d0fa
aarez.umatiya123@gmail.com <aarez.umatiya123@gmail.com>
2022-01-11 15:27:38 -0600

    Add project files.

---
+++ MyFirstProject/ViewViewModel/Controls/ControlsMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyFirstProject.Models;
+using MyFirstProject.ViewViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.ViewViewModel.Controls
+{
+    public class ControlsMenuViewModel : BaseViewModel
+    {
+        public ControlsMenuViewModel()
+        {
+            Title = Titles.ControlsTitle;
+        }
+    }
+}
